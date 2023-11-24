ee0ba6a772d502bea0d06cd68a95a6acd999d5c3
MOHAMED BELHASSEN <mohamed.belhassen@gmail.com>
2023-11-24 22:08:16 +0100

    ch2-example 1 -saving an  object when starting an activity

--- app/src/main/java/tn/creativeteam/myparsecourse/MainActivity.java
+++ app/src/main/java/tn/creativeteam/myparsecourse/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.parse.ParseObject;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ParseObject post = new ParseObject("Post");
+        post.put("body", "Hello, My name is Mohamed");
+        post.addAllUnique("tags", Arrays.asList("my-first-post", "welcome"));
+        post.put("numComments", 0);
+        post.saveInBackground();
     }
 }
