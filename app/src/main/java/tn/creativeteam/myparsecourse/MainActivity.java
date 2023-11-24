62a5f34a725089a40c400b8e82736e9241d1b358
MOHAMED BELHASSEN <mohamed.belhassen@gmail.com>
2023-11-24 22:31:09 +0100

    ch2-example 1 -saving an  object when starting an activity with feedback

--- app/src/main/java/tn/creativeteam/myparsecourse/MainActivity.java
+++ app/src/main/java/tn/creativeteam/myparsecourse/MainActivity.java
@@ -3,8 +3,11 @@ package tn.creativeteam.myparsecourse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 import java.util.Arrays;
 
@@ -14,11 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        String tag="MainActivity";
         ParseObject post = new ParseObject("Post");
         post.put("body", "Hello, My name is Mohamed");
         post.addAllUnique("tags", Arrays.asList("my-first-post", "welcome"));
         post.put("numComments", 0);
-        post.saveInBackground();
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if( e == null){
+                    Log.d(tag,"Post object well saved");
+                }else{
+                    Log.d(tag,"Error occured when saving Post object");
+                }
+            }
+        });
     }
 }
+
