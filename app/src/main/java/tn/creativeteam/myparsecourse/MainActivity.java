21b33749128a0132a24a713c5b8f324e6f9fa0ac
MOHAMED BELHASSEN <mohamed.belhassen@gmail.com>
2023-11-25 14:52:23 +0100

    ch2-example 5 - retrieve then delete an object

--- app/src/main/java/tn/creativeteam/myparsecourse/MainActivity.java
+++ app/src/main/java/tn/creativeteam/myparsecourse/MainActivity.java
@@ -14,18 +14,27 @@ import com.parse.SaveCallback;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String tag="MainActivity";
-
-        ParseObject post = ParseObject.createWithoutData("Post", "fFDrV82exD");
-        post.put("body","This object is updated without retrieving it");
-        post.increment("numComments");
-        post.addUnique("tags","updated-without-retrieval");
-        post.saveInBackground();
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Post");
+        query.getInBackground("fFDrV82exD", (post, e) -> {
+            if (e == null) {
+                //post.remove("numComments");
+                //post.deleteInBackground();
+                post.deleteInBackground(e1 -> {
+                    if(e == null){
+                        Log.d(tag,"post is well deleted");
+                    }else{
+                        Log.d(tag,"Error occured when deleting the post");
+                    }
+                });
+            } else {
+                Log.d(tag,"Error occured when retrieving the poqt");
+            }
+        });
     }
 }
 
