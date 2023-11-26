23a07b37f3723464d625205dd488a463693b2ae2
MOHAMED BELHASSEN <mohamed.belhassen@gmail.com>
2023-11-26 12:32:39 +0100

    ch2-example 10 - add another comment

--- app/src/main/java/tn/creativeteam/myparsecourse/MainActivity.java
+++ app/src/main/java/tn/creativeteam/myparsecourse/MainActivity.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.SaveCallback;
 
@@ -20,24 +22,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String tag="MainActivity";
 
-        ParseObject post = new ParseObject("Post");
-        post.put("body", "Hello, My name is Mohamed Belhassen");
-        post.addAllUnique("tags", Arrays.asList("my-first-post", "welcome"));
-        post.put("numComments", 0);
-        ParseObject comment = new ParseObject("Comment");
-        comment.put("message", "This is an awesome post with a comment");
-        comment.put("parent",post);
-        comment.saveInBackground(e -> {
-            if( e == null){
-                Log.d(tag,"**** Comment object well saved");
-                //comment should be saved before adding it to comments post relation
-                ParseRelation<ParseObject> comments = post.getRelation("comments");
-                comments.add(comment);
-                post.increment("numComments");
-                post.saveInBackground();
-            }else{
-                Log.d(tag,"**** Error occured when saving Post object");
-            }
+        ParseQuery<ParseObject> q= new ParseQuery<ParseObject>("Post");
+        q.getInBackground("ULDUvxFrd9", (post, e) -> {
+            ParseObject comment = new ParseObject("Comment");
+            comment.put("message", "This is the comment numner "+ (post.getInt("numComments")+1));
+            comment.put("parent",post);
+            comment.saveInBackground(e1 -> {
+                if( e1 == null){
+                    ParseRelation<ParseObject> comments = post.getRelation("comments");
+                    comments.add(comment);
+                    post.increment("numComments");
+                    post.saveInBackground();
+                }else{
+                    Log.d(tag,"**** Error occured when saving Post object");
+                }
+            });
         });
     }
 }
