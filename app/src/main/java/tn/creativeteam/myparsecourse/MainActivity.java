8ffb6a570cc7cc1a4a3019c99a71ff52bab8dd90
MOHAMED BELHASSEN <mohamed.belhassen@gmail.com>
2023-12-04 12:13:00 +0100

    ch2-ex12 delete_a_comment_from_post_relation

--- app/src/main/java/tn/creativeteam/myparsecourse/MainActivity.java
+++ app/src/main/java/tn/creativeteam/myparsecourse/MainActivity.java
@@ -16,21 +16,30 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    String tag="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String tag="MainActivity";
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Post");
-        query.getInBackground("fFDrV82exD", new GetCallback<ParseObject>() {
+        query.getInBackground("FmCUlJWC5h", new GetCallback<ParseObject>() {
             public void done(ParseObject post, ParseException e) {
                 if (e == null) {
                     ParseRelation<ParseObject> commentsRelation = post.getRelation("comments");
-                    ParseObject commentToDelete= ParseObject.createWithoutData("Post", "1zEcyElZ80");
+                    ParseObject commentToDelete= ParseObject.createWithoutData("Comment", "jANR9CEUKq");
                     commentsRelation.remove(commentToDelete);
+                    post.increment("numComments",-1);
+                    post.saveInBackground(exPost->{
+                        if(exPost == null){
+                            Log.d(tag,"comment removed from post");
+                        }else{
+                            Log.d(tag,"problem occured when removing the comment from relation");
+                        }
+                    });
+
                 } else {
-                    Log.d(tag,"Error occured when retrieving the poqt");
+                    Log.d(tag,"Error occured when retrieving the post");
                 }
             }
         });
