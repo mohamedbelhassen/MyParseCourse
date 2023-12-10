30084ee97611613f5d7c00c96491a1d4bfd3efd9
MOHAMED BELHASSEN <mohamed.belhassen@gmail.com>
2023-12-10 11:51:05 +0100

    ch4-ex3-user-login-with-alert-dialog

--- app/src/main/java/tn/creativeteam/myparsecourse/MainActivity.java
+++ app/src/main/java/tn/creativeteam/myparsecourse/MainActivity.java
@@ -1,9 +1,12 @@
 package tn.creativeteam.myparsecourse;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.parse.CountCallback;
 import com.parse.FindCallback;
@@ -27,22 +30,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String tag="MainActivity";
 
-        ParseUser user = new ParseUser();
-        //User required Fields
-        user.setUsername("Mohamed Belhassen");
-        user.setPassword("123");
-        user.setEmail("mohamed.belhassen@gmail.com");
-        //add some optional fields according your needs
-        user.put("phone", "97 100 000");
-        user.put("address", "Métouia, Gabes, Tunisia");
-
-        user.signUpInBackground(e -> {
+        ParseUser.logInInBackground("Mohamed Belhassen", "1233", (user, e) -> {
             if (e == null) {
-                Log.d(tag,"User account created successfully");
+                Log.d(tag,"User successfully logged in");
+                TextView tvEmail=findViewById(R.id.tv_email);
+                tvEmail.setText(ParseUser.getCurrentUser().getEmail());
             } else {
-                Log.e(tag,"Error encountered when creating user account: "+e.getMessage());
+                String error="Error: "+e.getMessage();
+                Log.e(tag,error);
+                showAlertDialog("Login Error",error,"Ok");
             }
         });
+    }
+
+    private void showAlertDialog(String Title, String Message, String buttonText)
+    {
+        AlertDialog.Builder alertDBuilder= new AlertDialog.Builder(this);
+        alertDBuilder.setMessage(Message)
+                .setCancelable(false)
+                .setTitle(Title)
+                .setPositiveButton(buttonText, (dialogInterface, i) -> {
+                    dialogInterface.dismiss();
+                    finish();});
+        AlertDialog alertDialog= alertDBuilder.create();
+        alertDialog.show();
     }
 
 
